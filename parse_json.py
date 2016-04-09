28fa907c35fdac6d7184c5a6591180c09f62180e
bundgus <bundgus@gmail.com>
2016-04-09 10:47:30 -0500

    now parsing sample file with one row per json record

--- parse_json.py
+++ parse_json.py
@@ -1,108 +1,6 @@
 import json
 
-
-class JSONGraphNode:
-    def __init__(self, nodename):
-        self.nodename = nodename
-        self.successors = []
-        self.attributes = {}
-        self.predecessor = None
-
-
-def create_key(prefix, element_name):
-    if prefix == '' or prefix is None:
-        return str(element_name)
-    else:
-        return str(prefix) + '__' + str(element_name)
-
-
-def getnodeattributes(jsonnode, graphnode, atpath=''):
-
-    if isinstance(jsonnode, dict):
-        for jnodekey in jsonnode:
-            jnode = jsonnode[jnodekey]
-
-            if isinstance(jnode, dict):
-                newatpath = create_key(atpath, jnodekey)
-                getnodeattributes(jnode, graphnode, atpath=newatpath)
-
-            elif not isinstance(jnode, list):
-                newkey = create_key(atpath, jnodekey)
-                graphnode.attributes[newkey] = str(jnode)
-    # logic for when a list has scalar values only, not a dictionary
-    # e.g. "emails": ["ccc@ccc.com"]
-    else:
-        #print(jsonnode)
-        graphnode.attributes[atpath] = str(jsonnode)
-
-
-def buildgraph(jsonnode, parentgraphnode, nodepath='', atpath=''):
-
-    if isinstance(jsonnode, dict):
-        for jnodekey in jsonnode:
-            jnode = jsonnode[jnodekey]
-            if isinstance(jnode, dict):
-                newnodepath = create_key(nodepath, jnodekey)
-                newatpath = create_key(atpath, jnodekey)
-                buildgraph(jnode, parentgraphnode, nodepath=newnodepath, atpath=newatpath)
-
-            elif isinstance(jnode, list):
-                jnode = dict(enumerate(jnode))
-                for key in jnode:
-                    newkey = nodepath + '__' + jnodekey[:] + str(key)
-                    newatpath = create_key(atpath, jnodekey[:])
-                    newigraphnode = JSONGraphNode(newkey)
-                    newigraphnode.predecessor = parentgraphnode
-                    parentgraphnode.successors.append(newigraphnode)
-                    getnodeattributes(jnode[key], newigraphnode, atpath=newatpath)
-                    buildgraph(jnode[key], newigraphnode, nodepath=newkey, atpath=newatpath)
-
-
-# here is where the udf function body starts
-def json_to_bag(jsonstring):
-
-
-    djson = json.loads(jsonstring)
-    if isinstance(djson, list):
-        djson = dict(enumerate(djson))
-
-    gn = JSONGraphNode('rootnode')
-    getnodeattributes(djson, gn, atpath='')
-    buildgraph(djson, gn)
-
-    # create denormalized rows from graph
-
-    def traversegraph(node_to_iterate, depth=0):
-        print('NODE: ', end='')
-        for i in range(depth):
-            print('....', end='')
-        print(node_to_iterate.nodename)
-        #if node_to_iterate.predecessor is not None:
-        #    print(node_to_iterate.predecessor.nodename)
-        for a in node_to_iterate.attributes:
-            print('              ', end='')
-            for i in range(depth-1):
-                print('    ', end='')
-            print(a, node_to_iterate.attributes[a])
-        print()
-        for suc in node_to_iterate.successors:
-            traversegraph(suc, depth=depth+1)
-
-    #traversegraph(gn)
-
-
-    denormrows = []
-
-    leafnodes = []
-    def findleafnodes(node_to_iterate):
-        if len(node_to_iterate.successors) < 1:
-            #print('found leaf node: ' + node_to_iterate.nodename)
-            leafnodes.append(node_to_iterate)
-        for suc in node_to_iterate.successors:
-            findleafnodes(suc)
-
-    findleafnodes(gn)
-    attributes = {
+attributes = {
     'ajax__actionCode': None,
     'ajax__componentType': None,
     'ancillarySelected__code': None,
@@ -210,6 +108,112 @@ def json_to_bag(jsonstring):
 
 }
 
+class JSONGraphNode:
+    def __init__(self, nodename):
+        self.nodename = nodename
+        self.successors = []
+        self.attributes = {}
+        self.predecessor = None
+
+
+def create_key(prefix, element_name):
+    if prefix == '' or prefix is None:
+        return str(element_name)
+    else:
+        return str(prefix) + '__' + str(element_name)
+
+
+def getnodeattributes(jsonnode, graphnode, atpath=''):
+
+    if isinstance(jsonnode, dict):
+        for jnodekey in jsonnode:
+            jnode = jsonnode[jnodekey]
+
+            if isinstance(jnode, dict):
+                newatpath = create_key(atpath, jnodekey)
+                getnodeattributes(jnode, graphnode, atpath=newatpath)
+
+            elif not isinstance(jnode, list):
+                newkey = create_key(atpath, jnodekey)
+                graphnode.attributes[newkey] = str(jnode)
+    # logic for when a list has scalar values only, not a dictionary
+    # e.g. "emails": ["ccc@ccc.com"]
+    else:
+        #print(jsonnode)
+        graphnode.attributes[atpath] = str(jsonnode)
+
+
+def buildgraph(jsonnode, parentgraphnode, nodepath='', atpath=''):
+
+    if isinstance(jsonnode, dict):
+        for jnodekey in jsonnode:
+            jnode = jsonnode[jnodekey]
+            if isinstance(jnode, dict):
+                newnodepath = create_key(nodepath, jnodekey)
+                newatpath = create_key(atpath, jnodekey)
+                buildgraph(jnode, parentgraphnode, nodepath=newnodepath, atpath=newatpath)
+
+            elif isinstance(jnode, list):
+                jnode = dict(enumerate(jnode))
+                for key in jnode:
+                    newkey = nodepath + '__' + jnodekey[:] + str(key)
+                    newatpath = create_key(atpath, jnodekey[:])
+                    newigraphnode = JSONGraphNode(newkey)
+                    newigraphnode.predecessor = parentgraphnode
+                    parentgraphnode.successors.append(newigraphnode)
+                    getnodeattributes(jnode[key], newigraphnode, atpath=newatpath)
+                    buildgraph(jnode[key], newigraphnode, nodepath=newkey, atpath=newatpath)
+
+
+# here is where the udf function body starts
+def json_to_bag(jsonstring, denormrows):
+
+    try:
+        if jsonstring != '':
+            djson = json.loads(jsonstring)
+        else:
+            return
+    except:
+        print('unparsable JSON')
+        print(jsonstring)
+        return
+    if isinstance(djson, list):
+        djson = dict(enumerate(djson))
+
+    gn = JSONGraphNode('rootnode')
+    getnodeattributes(djson, gn, atpath='')
+    buildgraph(djson, gn)
+
+    # create denormalized rows from graph
+
+    def traversegraph(node_to_iterate, depth=0):
+        print('NODE: ', end='')
+        for i in range(depth):
+            print('....', end='')
+        print(node_to_iterate.nodename)
+        #if node_to_iterate.predecessor is not None:
+        #    print(node_to_iterate.predecessor.nodename)
+        for a in node_to_iterate.attributes:
+            print('              ', end='')
+            for i in range(depth-1):
+                print('    ', end='')
+            print(a, node_to_iterate.attributes[a])
+        print()
+        for suc in node_to_iterate.successors:
+            traversegraph(suc, depth=depth+1)
+
+    #traversegraph(gn)
+
+    leafnodes = []
+    def findleafnodes(node_to_iterate):
+        if len(node_to_iterate.successors) < 1:
+            #print('found leaf node: ' + node_to_iterate.nodename)
+            leafnodes.append(node_to_iterate)
+        for suc in node_to_iterate.successors:
+            findleafnodes(suc)
+
+    findleafnodes(gn)
+
     def crawluptree(leafnode, masterdict):
         #print(leafnode.nodename)
         for at in leafnode.attributes:
@@ -225,17 +229,21 @@ def json_to_bag(jsonstring):
         #print(consolidateddict)
         denormrows.append(consolidateddict.copy())
 
-    for row in denormrows:
-        print(row)
+    #for row in denormrows:
+        # print(row)
+
+    return denormrows
+
+if __name__ == "__main__":
+    agdenormrows = []
+    filename = r'sample_json/analyticsWeb_SSW2010.2015-05-08-14_Original-businessRecord.json'
+    with open(filename, 'r') as f:
+        for line in f:
+            jstring = f.readline()
+            json_to_bag(jstring, agdenormrows)
 
     import csv
-    with open('analyticsweb.csv', 'w') as awf:
+    with open('analyticsweb.csv', 'a') as awf:
         w = csv.DictWriter(awf, attributes.keys(), lineterminator='\n')
         w.writeheader()
-        w.writerows(denormrows)
-
-
-filename = r'sample_json/businessRecord.json'
-with open(filename, 'r') as f:
-    jstring = f.read()
-    json_to_bag(jstring)
+        w.writerows(agdenormrows)
