4fb9dfd0afe8f5db49dd95f2dd04b4d826f60e84
bundgus <bundgus@gmail.com>
2016-04-12 22:32:47 -0500

    writes _flattened_keys.json to output directory

--- curate_json.py
+++ curate_json.py
@@ -1,95 +1,122 @@
-from __future__ import with_statement
 import json
-import networkx as nx
-import io
-import datetime
-from create_avro_schema import createavroschema
-from create_json_to_parquet_pig_script import create_json_to_parquet_pig_script
-from create_pig_udf import create_pig_udf
-from create_json_schema import create_json_schema
-from create_schema_outline import create_schema_outline
-from create_flattened_keys import create_flattened_keys
+import uuid
 
-debugflag = False
+class JSONGraphNode:
+    def __init__(self, nodename):
+        self.nodename = nodename
+        self.successors = []
+        self.attributes = {}
+        self.predecessor = None
 
 
-def debug(debugmessage):
-    if debugflag is True:
-        print(debugmessage)
+def create_key(prefix, element_name):
+    if prefix == '' or prefix is None:
+        return str(element_name)
+    else:
+        return str(prefix) + '__' + str(element_name)
 
 
-def drilldict(jsonnode, myG, nodename, nodeprefix):
-    for key in jsonnode:
-        if isinstance(jsonnode[key], dict):
-            debug(key + 'found a dict inside a dict')
-            if nodeprefix == '':
-                drilldict(jsonnode[key], myG, nodename, key)
-            else:
-                drilldict(jsonnode[key], myG, nodename, nodeprefix + '__' + key)
-        elif isinstance(jsonnode[key], list):
-            debug('found a list inside a dict ' + key)
-            if nodeprefix == '':
-                myG.add_edge(nodename, key)
-                drilllist(jsonnode[key], myG, key, key)
-            else:
-                myG.add_edge(nodename, nodeprefix + '__' + key)
-                drilllist(jsonnode[key], myG, nodeprefix + '__' + key, nodeprefix)
+def getnodeattributes(jsonnode, graphnode, atpath=''):
+
+    if isinstance(jsonnode, dict):
+        for jnodekey in jsonnode:
+            jnode = jsonnode[jnodekey]
+
+            if isinstance(jnode, dict):
+                newatpath = create_key(atpath, jnodekey)
+                getnodeattributes(jnode, graphnode, atpath=newatpath)
+
+            elif not isinstance(jnode, list):
+                newkey = create_key(atpath, jnodekey)
+                graphnode.attributes[newkey] = str(jnode)
+    # logic for when a list has scalar values only, not a dictionary
+    # e.g. "emails": ["ccc@ccc.com"]
+    else:
+        #print(jsonnode)
+        graphnode.attributes[atpath] = str(jsonnode)
+
+
+def buildgraph(jsonnode, parentgraphnode, nodepath='', atpath=''):
+
+    if isinstance(jsonnode, dict):
+        for jnodekey in jsonnode:
+            jnode = jsonnode[jnodekey]
+            if isinstance(jnode, dict):
+                newnodepath = create_key(nodepath, jnodekey)
+                newatpath = create_key(atpath, jnodekey)
+                buildgraph(jnode, parentgraphnode, nodepath=newnodepath, atpath=newatpath)
+
+            elif isinstance(jnode, list):
+                jnode = dict(enumerate(jnode))
+                for key in jnode:
+                    newkey = nodepath + '__' + jnodekey[:] + str(key)
+                    newatpath = create_key(atpath, jnodekey[:])
+                    newigraphnode = JSONGraphNode(newkey)
+                    newigraphnode.predecessor = parentgraphnode
+                    parentgraphnode.successors.append(newigraphnode)
+                    getnodeattributes(jnode[key], newigraphnode, atpath=newatpath)
+                    buildgraph(jnode[key], newigraphnode, nodepath=newkey, atpath=newatpath)
+
+
+masterdict = {}
+
+
+def curate_json(jsonstring):
+    denormrows = []
+    jsonuuid = str(uuid.uuid4())
+
+    try:
+        if jsonstring != '':
+            djson = json.loads(jsonstring)
         else:
-            debug('found an element inside a dict ' + nodeprefix + ' ' + key)
-            if nodeprefix == '':
-                myG.node[nodename][key] = jsonnode[key]
-            else:
-                myG.node[nodename][nodeprefix + '__' + key] = jsonnode[key]
+            return
+    except:
+        raise Exception('unparsable JSON: ' + jsonstring)
+    if isinstance(djson, list):
+        djson = dict(enumerate(djson))
 
+    gn = JSONGraphNode('rootnode')
+    getnodeattributes(djson, gn, atpath='')
+    buildgraph(djson, gn)
 
-def drilllist(jsonnode, myG, nodename, nodeprefix):
-    for i, wnode in enumerate(jsonnode):
-        if isinstance(wnode, dict):
-            debug('found a dict inside a list' + ' ' + nodename + ' ' + nodeprefix)
-            if nodeprefix == '':
-                drilldict(jsonnode[i], myG, nodename, nodename)
-            else:
-                drilldict(jsonnode[i], myG, nodename, nodename)
-        elif isinstance(wnode, list):
-            debug('found a list inside a list', nodename, nodeprefix)
-        else:
-            # TODO: add logic for when a list has scalar values only, not a dictionary
-            #print('found an element inside a list ' + str(wnode))
-            pass
+    leafnodes = []
 
+    def findleafnodes(node_to_iterate):
+        if len(node_to_iterate.successors) < 1:
+            leafnodes.append(node_to_iterate)
+        for suc in node_to_iterate.successors:
+            findleafnodes(suc)
 
-def create_flattened_list(graph, nodename='rootnode', parts={}):
-    for lkey in graph.node[nodename]:
-        parts[lkey] = None
-    for succe in graph.successors(nodename):
-        create_flattened_list(graph, nodename=succe, parts=parts)
+    findleafnodes(gn)
 
-    pk = parts.keys()
-    pkl = list(pk)
-    pkl.sort()
-    return pkl
+    def crawluptree(leafnode):
+        for at in leafnode.attributes:
+            masterdict[at] = leafnode.attributes[at]
+        if leafnode.predecessor is not None:
+            crawluptree(leafnode.predecessor)
+        masterdict['json_uuid'] = jsonuuid
+        return masterdict
+
+    for ln in leafnodes:
+        consolidateddict = crawluptree(ln)
+        denormrows.append(consolidateddict.copy())
+
+    return denormrows
 
 if __name__ == "__main__":
-
-    prefixname = 'businessRecord'
-    #filename = r'sample_json/analyticsWeb_SSW2010.2015-05-08-14_Original-businessRecord.json'
     filename = r'sample_json/v12-businessRecord.json'
 
-    # First let's build a digraph of lists from the JSON samples (one JSON of the same schema type per line)
-    G = nx.DiGraph()
+    agdenormrows =[]
     with open(filename, 'r') as f:
-        G.add_node('rootnode')
-        for rawjson in f:
-            rjson = json.loads(rawjson)
-            # start at the root node of the JSON
-            drilldict(rjson, G, 'rootnode', '')
+        for line in f:
+            jstring = f.readline()
+            denormrows = curate_json(jstring)
+            if denormrows is not None:
+                agdenormrows.extend(denormrows)
 
-    # get flattened list of elements
-    partlist = create_flattened_list(G)
+    for key in masterdict:
+        masterdict[key] = None
 
-    create_json_schema(G, prefixname=prefixname)
-    #createavroschema(G, 'rootnode')
-    create_json_to_parquet_pig_script(G, parts=partlist, prefixname=prefixname)
-    create_pig_udf(G, parts=partlist, prefixname=prefixname)
-    create_schema_outline(G)
-    create_flattened_keys(partlist, prefixname=prefixname)
+    with open(r'output/businessRecord_flattened_keys.json', 'w') as fk:
+        fk.write(json.dumps(masterdict, sort_keys=True, indent=4, separators=(',', ': ')))
+    print('flat schema written to output/businessRecord_flattened_keys.json')
