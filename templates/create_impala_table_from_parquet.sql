403031efc65d82f567429a30f3de5a6a4118d446
bundgus <bundgus@gmail.com>
2017-09-13 15:35:51 -0500

    remove unneeded files.  update .gitignore

--- templates/create_impala_table_from_parquet.sql
+++
@@ -1,4 +0,0 @@
-CREATE EXTERNAL TABLE mlb_udf_parq_test
-LIKE PARQUET '/user/sg217516/ecommerce_checkin/transformed_json_logs/businessRecord.parq/part-m-00000.parquet'
-  STORED AS PARQUET
-  LOCATION '/user/sg217516/ecommerce_checkin/transformed_json_logs/businessRecord.parq';
